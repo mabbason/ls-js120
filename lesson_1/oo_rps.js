958cfb62452e8ff4f68dd5dc0bc4766b14cdd74b
Miles Abbason <miles.abbason@gmail.com>
2021-07-08 15:45:48 -0400

    OO rps game, ready for review

--- lesson_1/oo_rps.js
+++ lesson_1/oo_rps.js
@@ -1,20 +1,3 @@
-/*
-# Write a textual description of the problem or exercise
-  RPS is a two-player game where each player chooses one of
-  three possible moves: rock, paper, or scissors. The winner
-  is chosen by comparing their moves with the following rules:
-
-  - Rock crushes scissors, i.e., rock wins against scissors.
-  - Scissors cuts paper, i.e., scissors beats paper.
-  - Paper wraps rock, i.e., paper beats rock.
-  - If the players chose the same move, the game is a tie.
-
-# Extract the significant nouns and verbs from the description
-  Nouns: player, move, rule
-  Verbs: choose, compare
-
-# Organize and associate the verbs with the nouns
-*/
 const readline = require('readline-sync');
 
 const RPSGame = {
